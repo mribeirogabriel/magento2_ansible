80f59c1a5fca3e37b7097f4dc050030a8e2d6fe6
marco <mribeirogabriel@hotmail.com>
2020-03-09 10:30:22 -0300

    Versão final

--- magento.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE magento;
-CREATE USER 'magentouser'@'localhost' IDENTIFIED BY 'password_here';
-GRANT ALL ON magento.* TO 'magentouser'@'localhost' IDENTIFIED BY 'password_here' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
